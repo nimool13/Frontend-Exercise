32076bc571b1350ae3cbef63c43a0543bfd9e247
nimool13 <nimasafavi3@gmail.com>
2023-02-05 21:18:49 +0100

    Third cm- editing side nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,26 @@
+
+//sidenav init
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
 
+  // Parallax page
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+  });
 
+  // side nav
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+  let query = document.querySelector('.q1');
+  let searchBtn= document.queryselector('.searchBtn');
+  
+  searchBtn.onclick = function(){
+    let url='https://www.google.com/search?q'+ query.value;
+    window.open(url)
+  }
